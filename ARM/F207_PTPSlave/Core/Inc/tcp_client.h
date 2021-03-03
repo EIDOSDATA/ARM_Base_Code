ecf125016defbe1b5d292898accb9c2088fe8c9e
EIDOSDATA <ihjisan00@gmail.com>
2021-03-03 15:08:01 +0900

    Purge and Create New Repo

--- ARM/F207_PTPSlave/Core/Inc/tcp_client.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TCP_CLIENT_H_
-#define _TCP_CLIENT_H_
-
-#include "stm32f2xx_hal.h"
-#include "cmsis_os.h"
-
-void tcp_client_task(void const * argument);
-
-#endif
--- ARM/F429_PTPSlave/Debug/Startup/startup_stm32f429igtx.d
+++
@@ -1,1 +0,0 @@
-Startup/startup_stm32f429igtx.o: ../Startup/startup_stm32f429igtx.s
